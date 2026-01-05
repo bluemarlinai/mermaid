75d7a659b7d464099722fa96e31ac35b41c88430
您的新用户名 <>
2026-01-05 08:41:44 +0800

    feat(编辑器): 添加Mermaid语法高亮并重构工具栏布局
    - 为Monaco编辑器注册Mermaid语言并实现语法高亮
    - 将工具栏按钮重新组织到编辑器上方和图表下方
    - 添加编辑器工具栏的CSS样式
    - 移除重复的工具栏按钮并调整位置

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,69 @@
 import React, { useEffect, useState, useRef } from 'react';
 import Editor from '@monaco-editor/react';
+import * as monaco from 'monaco-editor';
 import mermaid from 'mermaid';
 import './App.css';
+
+// Define Mermaid syntax highlighting using Monarch
+monaco.languages.register({
+  id: 'mermaid'
+});
+
+// Basic Mermaid syntax highlighting rules
+monaco.languages.setMonarchTokensProvider('mermaid', {
+  tokenizer: {
+    root: [
+      // Diagram types
+      [/\b(flowchart|sequenceDiagram|classDiagram|stateDiagram|stateDiagram-v2|gantt|pie|erDiagram)\b/, 'keyword'],
+      
+      // Keywords and directives
+      [/\b(title|dateFormat|section|participant|as|->>|-->>|->|-->|note|activate|deactivate|loop|alt|else|opt|par|and|end|class|abstract|interface|extends|implements|namespace|package|enum|direction|graph|TB|BT|LR|RL)\b/, 'keyword'],
+      
+      // Operators and arrows
+      [/->>|-->>|->|-->/, 'operator'],
+      [/[=<>|]/, 'operator'],
+      
+      // Comments
+      [/\/\//, 'comment', '@comment'],
+      
+      // Strings
+      [/'[^']*'/, 'string'],
+      [/"[^"]*"/, 'string'],
+      
+      // Numbers
+      [/\b\d+\b/, 'number'],
+      
+      // Identifiers
+      [/[a-zA-Z_][a-zA-Z0-9_]*/, 'identifier']
+    ],
+    comment: [
+      [/[^\/]/, 'comment'],
+      [/\/\//, 'comment', '@push'],
+      [/\/\//, 'comment', '@pop'],
+      [/^\s*\*/, 'comment'],
+      [/\*\//, 'comment', '@pop']
+    ]
+  }
+});
+
+// Language configuration
+monaco.languages.setLanguageConfiguration('mermaid', {
+  comments: {
+    lineComment: '//'
+  },
+  brackets: [
+    ['[', ']'],
+    ['(', ')'],
+    ['{', '}']
+  ],
+  autoClosingPairs: [
+    { open: '[', close: ']' },
+    { open: '(', close: ')' },
+    { open: '{', close: '}' },
+    { open: "'", close: "'" },
+    { open: '"', close: '"' }
+  ]
+});
 
 // Template code for diagram types
 const diagramTemplates = {
@@ -65,7 +127,7 @@ const App: React.FC = () => {
 
   // Monaco Editor options
   const editorOptions: any = {
-    language: 'markdown', // We'll use markdown which has good support for code blocks
+    language: 'mermaid', // Use our custom mermaid language
     theme: 'vs-light',
     fontSize: 14,
     fontFamily: 'Monaco, Menlo, Ubuntu Mono, monospace',
@@ -255,38 +317,6 @@ const App: React.FC = () => {
     <div className="app-container">
       <header className="app-header">
         <h1>Mermaid Online Editor</h1>
-        <div className="toolbar">
-          <button id="copy-btn" className="toolbar-btn" title="Copy Code" onClick={copyCode}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
-              <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
-            </svg>
-          </button>
-          <button id="download-svg-btn" className="toolbar-btn" title="Download SVG" onClick={downloadSvg}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-              <path d="M4.5 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
-            </svg>
-          </button>
-          <button id="download-png-btn" className="toolbar-btn" title="Download PNG" disabled>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-              <path d="M6.5 10.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
-              <path d="M3.051 3.15a2.5 2.5 0 0 1 3.89-1.005l-.002.003A2.5 2.5 0 0 1 8.005 5H8a2.5 2.5 0 0 1 2.5-2.5v.003a2.5 2.5 0 0 1 1.77 4.208 2.5 2.5 0 0 1-.586.856 2.5 2.5 0 0 1-3.764 0 2.5 2.5 0 0 1-.586-.856A2.5 2.5 0 0 1 5.5 5.003V5a2.5 2.5 0 0 1-2.449-2.85z"/>
-              <path d="M12 9a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-              <path d="M9.5 9a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-              <path d="M6.5 9a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-              <path d="M12 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-              <path d="M9.5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-              <path d="M6.5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-            </svg>
-          </button>
-          <button id="clear-btn" className="toolbar-btn" title="Clear" onClick={clearEditor}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-            </svg>
-          </button>
-        </div>
       </header>
 
       <div className="app-main">
@@ -301,11 +331,24 @@ const App: React.FC = () => {
               <option value="gantt">Gantt Chart</option>
               <option value="pie">Pie Chart</option>
             </select>
+            <div className="editor-toolbar">
+              <button id="copy-btn" className="toolbar-btn" title="Copy Code" onClick={copyCode}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                  <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+                  <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+                </svg>
+              </button>
+              <button id="clear-btn" className="toolbar-btn" title="Clear" onClick={clearEditor}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                </svg>
+              </button>
+            </div>
           </div>
           <div className="editor-content">
             <Editor
               height="100%"
-              language="markdown"
+              language="mermaid"
               value={code}
               options={editorOptions}
               onChange={handleEditorChange}
@@ -325,6 +368,25 @@ const App: React.FC = () => {
             <button id="zoom-in" className="toolbar-btn" title="Zoom In" onClick={zoomIn}>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                 <path d="M7.5 14a.5.5 0 0 0 .5-.5v-1h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1v-1a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1h-1a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1v1a.5.5 0 0 0 .5.5h1zM3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zm10 1a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V1z"/>
+              </svg>
+            </button>
+            <button id="download-svg-btn" className="toolbar-btn" title="Download SVG" onClick={downloadSvg}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                <path d="M4.5 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
+              </svg>
+            </button>
+            <button id="download-png-btn" className="toolbar-btn" title="Download PNG" disabled>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                <path d="M6.5 10.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+                <path d="M3.051 3.15a2.5 2.5 0 0 1 3.89-1.005l-.002.003A2.5 2.5 0 0 1 8.005 5H8a2.5 2.5 0 0 1 2.5-2.5v.003a2.5 2.5 0 0 1 1.77 4.208 2.5 2.5 0 0 1-.586.856 2.5 2.5 0 0 1-3.764 0 2.5 2.5 0 0 1-.586-.856A2.5 2.5 0 0 1 5.5 5.003V5a2.5 2.5 0 0 1-2.449-2.85z"/>
+                <path d="M12 9a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                <path d="M9.5 9a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                <path d="M6.5 9a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                <path d="M12 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                <path d="M9.5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                <path d="M6.5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
               </svg>
             </button>
             <button id="fullscreen-btn" className="toolbar-btn" title="Fullscreen" onClick={toggleFullscreen}>
